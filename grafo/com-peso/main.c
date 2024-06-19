a99e37a6480ad4c67273eff3d8d577227220ca1f
Vitor Silva <vitor.gabriel.vinielle@gmail.com>
2024-06-19 10:41:48 -0300

    Começando ex 3

---
+++ grafo/com-peso/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "grafoM.h"
+#include "grafoL.h"
+
+int main()
+{
+    int v = 10;
+
+    GrafoM* M = criaGrafoMatriz(v);
+    GrafoL* L = criaGrafoLista(v);
+    
+    criaAdjacenciaM(M, 1, 2, 1);
+    criaAdjacenciaM(M, 4, 2, 1);
+    criaAdjacenciaM(M, 2, 1, 2);
+    criaAdjacenciaL(L, 1, 2, 3);
+    criaAdjacenciaL(L, 3, 1, 4);
+
+    printf("Grafo com Matriz de Adjacencia: \n");
+    imprimeMatrizAdj(M);
+    printf("Arestas Incidentes de %d: %d\n", 2, arestasIncidentes(M, 2));
+    printf("Grafo com Lista de Adjacencia: \n");
+    imprimeListaAdj(L);
+
+    freeGrafoM(M);
+    freeGrafoL(L);
+    return 0;
+}
